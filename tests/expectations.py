b2d80064f11dbd3dc18df3fa5050d95b94a066e5
tserg <8017125+tserg@users.noreply.github.com>
2023-02-10 17:24:01 +0800

    add tests

--- tests/expectations.py
+++ tests/expectations.py
@@ -3,4 +3,7 @@ EXPECTED = [
     ("dead_store/call", {}),
     ("dead_store/annassign", {"Dead Store": 1}),
     ("dead_store/annassign_2", {"Dead Store": 1}),
+    ("unused_param/single", {"Unused Function Parameter": 1}),
+    ("unused_param/double", {"Unused Function Parameter": 2}),
+    ("unused_param/namespace_single", {"Unused Function Parameter": 1}),
 ]
