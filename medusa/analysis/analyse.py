b6cd99ff998942ee81a81664d23010ded79a3afd
tserg <8017125+tserg@users.noreply.github.com>
2023-02-10 17:25:53 +0800

    fix lint

--- medusa/analysis/analyse.py
+++ medusa/analysis/analyse.py
@@ -3,10 +3,7 @@ from vyper import ast as vy_ast
 from medusa.analysis.base import BaseAnalyser
 from medusa.analysis.passes import DeadStoreAnalyser, UnusedParamAnalyser
 
-PASSES = {
-    "Dead Store": DeadStoreAnalyser,
-    "Unused Function Parameter": UnusedParamAnalyser,
-}
+PASSES = {"Dead Store": DeadStoreAnalyser, "Unused Function Parameter": UnusedParamAnalyser}
 
 
 def analyse(ast: vy_ast.Module) -> dict[BaseAnalyser, set[vy_ast.VyperNode]]:
