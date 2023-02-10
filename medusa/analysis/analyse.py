1faec1838997dd154a91582f69d470df6cb2e01e
tserg <8017125+tserg@users.noreply.github.com>
2023-02-10 17:23:46 +0800

    add unused param pass

--- medusa/analysis/analyse.py
+++ medusa/analysis/analyse.py
@@ -1,9 +1,12 @@
 from vyper import ast as vy_ast
 
 from medusa.analysis.base import BaseAnalyser
-from medusa.analysis.passes import DeadStoreAnalyser
+from medusa.analysis.passes import DeadStoreAnalyser, UnusedParamAnalyser
 
-PASSES = {"Dead Store": DeadStoreAnalyser}
+PASSES = {
+    "Dead Store": DeadStoreAnalyser,
+    "Unused Function Parameter": UnusedParamAnalyser,
+}
 
 
 def analyse(ast: vy_ast.Module) -> dict[BaseAnalyser, set[vy_ast.VyperNode]]:
